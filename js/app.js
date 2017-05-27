0036019c9938f4c9ba868ff5032dc7c3bbca60d8
Lee Molton <leemolton@hotmail.co.uk>
2017-05-27 14:55:31 +0100

    Initialize NPM

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//Define an angular module for our app
+
+angular.module('sampleApp', ['ngRoute', 'RouteControllers']);
+ 
+angular.module('sampleApp').config(function($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);  // Enable href routing without hashes
+ 
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: "HomeController"
+    })
+    .when('/about', {
+        templateUrl: 'templates/about.html',
+        controller: "HomeController"
+    })
+    .when('/contact', {
+    	templateUrl: 'templates/contact.html',
+        controller: "HomeController"
+    });
+});
